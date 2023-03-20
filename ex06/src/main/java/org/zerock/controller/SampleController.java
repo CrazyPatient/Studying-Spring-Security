b0d2209ce18b72bb66a75db6e106aeae3118ac92
HKT <HKT@192.168.2.113>
2023-03-20 23:58:22 +0900

    make SampleController

---
+++ ex06/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,12 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/sample/*")
+@Controller
+
+
+public class SampleController {
+
+}
